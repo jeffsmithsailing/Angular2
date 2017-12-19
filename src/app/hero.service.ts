27522175e5d41b3fb943f08ced83bc2ea47494d0
Jeffrey Smith <jeffreysmithixd@gmail.com>
2017-12-19 12:56:47 -0800

    Fixed syntax error - all upper case URL converted to Url in hero.service.ts. Working perfectly

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,9 +4,9 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 import { Hero } from './hero';
-/**import { HEROES } from './mock-heroes';*/
 import { MessageService } from './message.service';
 import {HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError , map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
@@ -16,15 +16,44 @@ export class HeroService {
 	private messageService: MessageService) { }
 
   /** Get Heroes from the Server */
-	getHeroes(): Observable<Hero[]> {
-		return this.http.get<Hero[]>(this.heroesURL)
+	getHeroes (): Observable<Hero[]> {
+		return this.http.get<Hero[]>(this.heroesUrl)
+			.pipe(
+				tap(heroes => this.log(`fetched heroes`)),
+				catchError(this.handleError('getHeroes', []))
+				);
   }
 
+	/** Get hero by ID - will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // Todo: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+	return this.http.get<Hero>(url).pipe(
+		tap(_ => this.log(`fetched hero id=${id}`)),
+		catchError(this.handleError<Hero>(`getHero id=${id}`))
+	);  
   }
+	
+	/**
+	* Handle Http operation that failed.
+	* Let the app continue.
+	* @param operation - name of the operation that failed
+	* @param result - optional value to return as the observable result
+	*/
+	private handleError<T> (operation = 'operation', result?: T) {
+		return (error: any): Observable<T> => {
+			
+			//TODO: send the error to remote logging infrastructure
+			console.error(error); // log to console instead
+			
+			//TODO: better job of transforming error for user consumption
+			this.log('${operation} failed: ${error.message}');
+			
+			// Let the app keep running by returning an empty result.
+			return of(result as T);
+		};
+		
+		
+	}
 	
 	/** Log a Hero Service message with the MessageService */
 	private log(message: string){
