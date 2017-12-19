fda2d5793ae87cf39372e3aa60a431c343acb6e7
Jeffrey Smith <jeffreysmithixd@gmail.com>
2017-12-19 08:33:46 -0800

    Working perfectly. App-routing.module.ts had defects. Once fixed the entire app started working correctly.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
@@ -20,6 +19,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
